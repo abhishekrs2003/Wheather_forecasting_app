5ab1122379a1e4ff9fe1c4ef5134b18874862185
abhishekrs2003 <rsabhishek98@gmail.com>
2026-05-21 18:17:51 +0530

    upated

--- app.py
+++ app.py
@@ -1,37 +1,17 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # Page title
 st.title("Weather Forecasting App")
 
 # Load CSV file
-df = pd.read_csv("data/weather_data.csv")
+data = pd.read_csv("data/weather_data.csv")
 
-# Show dataframe
-st.subheader("Weather Dataset")
-st.dataframe(df)
+data['datetime'] = pd.to_datetime(data['date'])
+st.line_chart(data, x='datetime', y="rain")
 
-# Basic information
-st.subheader("Dataset Information")
-st.write(df.describe())
-
-# Show temperature chart
-st.subheader("Temperature Chart")
-st.line_chart(df["temperature_2m"])
-
-# Show rain chart
-st.subheader("Rain Chart")
-st.bar_chart(df["rain"])
-
-# Select columns
-st.subheader("Custom Column Viewer")
-
-column = st.selectbox(
-    "Select a column",
-    df.columns
-)
-
-st.write(df[column])
-
-# Success message
-st.success("Weather data loaded successfully!")
+st.subheader("Froecasted Whether metrics for Tommorow")
+col1,col2 = st.columns(2)
+col1.metric("Temperature ", np.round(data["temperature_2m"].mean(),2))
+col2.metric("Rain", np.round(data["rain"].mean(),2))
